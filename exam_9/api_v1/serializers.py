35bf449f75268353b46538906b8a149e03d2d9f6
belek <belektolubaev@gmail.com>
2019-04-04 19:58:43 +0600

    Добавлено serializers

---
+++ exam_9/api_v1/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from webapp.models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:movie-detail')
+
+    class Meta:
+        model = Movie
+        fields = ('url', 'id', 'name', 'description', 'poster', 'release_date', 'finish_date')
